c097d38e309f002fb8afd649eb8b2eab107b44cc
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-07-01 09:47:22 -0700

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: false
+
+# artist class
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def new_song(song_name, genre)
+    Song.new(song_name, self, genre)
+  end
+
+  def genres
+    songs.map { |song| song.genre } 
+  end
+    
+end
