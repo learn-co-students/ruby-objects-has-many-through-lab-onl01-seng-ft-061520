7700ef12a2ed86cd1595a6bbb1c43ce878e6a061
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-01 21:44:10 +0000

    Done.

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,13 +13,17 @@ class Patient
     @@all 
   end 
   
-  def appointment 
-    Appointment.all.select {|appointment| appointment.patient == self}
+  def appointments
+    Appointment.all.select { |appointment| appointment.patient == self }
   end
   
   def new_appointment(date, doctor)
     Appointment.new(date, self, doctor)
-  end 
+  end
+  
+   def doctors
+    self.appointments.collect {|appointment| appointment.doctor}
+  end
 
   
 end 
